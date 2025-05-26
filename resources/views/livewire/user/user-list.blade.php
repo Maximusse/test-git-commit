0abfda242ad203ba351ffc8b8e180f10f0c17535
Maximusse <dekomaximusse@gmail.com>
2025-05-26 11:51:35 +0000

    test

--- resources/views/livewire/user/user-list.blade.php
+++ resources/views/livewire/user/user-list.blade.php
@@ -1,3 +1,7 @@
 <div>
-    <h1>User</h1>
+    <h1>{{$x}}</h1>
+    <div>
+        <button wire:click='increment'>Incrémenter +</button>
+        <button wire:click='decrement'>Décrémenter -</button>
+    </div>
 </div>
